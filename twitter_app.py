cf684109e065a1ad89187ea767bf7538b8ab1290
satheesh2k14 <satheesh.prabhakaran97@gmail.com>
2017-03-15 23:57:59 +0530

    Sentiment analysis tweepy TextBlob

---
+++ twitter_app.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 19:50:55 2017
+
+@author: Satheesh
+"""
+
+import tweepy
+
+from textblob import TextBlob
+
+consumer_key =	'howuDVCDR8f63eYCnSIrm4kX2'
+consumer_secret = 'DvfottTuO81adluMbt3SBNV2pgAZLKhLJmbBPxoARQfOmHpgz9'
+
+access_token = '2874878936-1t1dgKcJPXZ65pVxsStL1zFLDqY63F92d0e4Cbx'
+access_token_secret = '2A6OUFB52ic7D6eGRB4Um8lqo4n7kmzz4YNuUmXRkf1Dl'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('TRUMP')
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
